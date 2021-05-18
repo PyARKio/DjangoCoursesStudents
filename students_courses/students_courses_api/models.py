29ae3b2793092c68ea92cd4092057a6180dcaf64
PyARKio <fedoretss@gmail.com>
2021-05-18 15:34:26 +0300

    Create models, add one student

---
+++ students_courses/students_courses_api/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=120)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class CourseParticipant(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.course
